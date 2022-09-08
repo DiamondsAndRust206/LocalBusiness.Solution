302430c3529f1143820b446f0573f65a3b9ffcdc
Timothy RYAN <ryaninlux@gmail.com>
2022-09-08 16:10:37 -0700

    Add models and reconfigure startup file

--- LocalBusinessApi/Startup.cs
+++ LocalBusinessApi/Startup.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using LocalBusinessApi.Models;
 
 namespace LocalBusinessApi
 {
