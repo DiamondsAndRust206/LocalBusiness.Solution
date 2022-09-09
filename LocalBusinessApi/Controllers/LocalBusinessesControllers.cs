6c2791b23a891265faa348293820e9ffed3c5a40
Timothy RYAN <ryaninlux@gmail.com>
2022-09-08 17:25:52 -0700

    Add LocalBusinesses controller and begin to add swagger

---
+++ LocalBusinessApi/Controllers/LocalBusinessesControllers.cs
@@ -0,0 +1,98 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LocalBusinessApi.Models;
+using System.Linq;
+
+namespace LocalBusinessApi.Controllers
+{
+  
+  [Route("api/[controller]")]
+  [ApiController]
+  public class LocalBusinessesController :ControllerBase
+  {
+    private readonly LocalBusinessContext _db;
+    public LocalBusinessesController(LocalBusinessContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<List<LocalBusiness>> Get(string type, int rating)
+    {
+
+      IQueryable<LocalBusiness> query = _db.LocalBusinesses.AsQueryable();
+      if(type != null)
+      {
+        query = query.Where(e => e.Type == type);
+      }
+      if(rating > 0)
+      {
+        query = query.Where(e => e.Rating >= rating);
+      }
+      return await query.ToListAsync();
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<LocalBusiness>> GetBusiness(int id)
+    {
+      var localbusiness = await _db.LocalBusinesses.FindAsync(id);
+      if(localbusiness == null)
+      {
+        return NotFound();
+      }
+      return localbusiness;
+
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<LocalBusiness>> Post(LocalBusiness localbusiness)
+    {
+      _db.LocalBusinesses.Add(localbusiness);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetBusiness),new{id=localbusiness.LocalBusinessId},localbusiness);
+    } 
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Put(int id,LocalBusiness localBusiness)
+    { 
+      if(id!=localBusiness.LocalBusinessId)
+      {
+        return BadRequest();
+      }
+      _db.Entry(localBusiness).State =EntityState.Modified;
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch(DbUpdateConcurrencyException)
+      {
+        if(!LocalBusinessExists(id))
+        {
+          return  NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+
+    }
+    private bool LocalBusinessExists(int id )
+    {
+      return _db.LocalBusinesses.Any(entry=>entry.LocalBusinessId ==id);
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+      var localBusiness =await _db.LocalBusinesses.FindAsync(id);
+      if(localBusiness == null)
+      {
+        return NotFound();
+      }
+      _db.LocalBusinesses.Remove(localBusiness);
+      await _db.SaveChangesAsync();
+      return NoContent();
+    }
+  }
+}
---
+++ LocalBusinessApi/Migrations/20220908235437_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBusinessApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LocalBusinesses",
+                columns: table => new
+                {
+                    LocalBusinessId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Owner = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Type = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Location = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalBusinesses", x => x.LocalBusinessId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocalBusinesses");
+        }
+    }
+}
